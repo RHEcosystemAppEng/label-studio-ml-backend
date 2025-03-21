2a770f68df5472f3303e80d5de6732d570874a46
rhkp <kpanchal@redhat.com>
2025-03-21 12:11:49 -0400

    Add import tasks from local file, export to json

--- label_studio_ml/examples/notebooks/ls_helper.py
+++ label_studio_ml/examples/notebooks/ls_helper.py
@@ -1,5 +1,9 @@
 from json import JSONDecodeError
 from label_studio_sdk.core.api_error import ApiError
+from datetime import datetime
+import requests
+import uuid
+import os
 
 def get_project_by_title(ls, project_title):
     projects = ls.projects.list()
@@ -59,3 +63,47 @@ def get_or_create_model(ls,
                 url=url,
                 project=project_id,
                 is_interactive=is_interactive)
+
+def export_annotated_data(project_id, ls_url, api_key):
+    # Get the export of the annotations
+    url = ls_url + "/api/projects/" + str(project_id) + "/export"
+    print(url)
+    token = 'Token ' + api_key
+    print(token)
+    Headers = { 'Authorization' : token }
+    response = requests.get(url, headers=Headers )
+    return response
+
+def get_timestamp():
+    return datetime.timestamp(datetime.now())
+
+def get_random_chars(length=4):
+    random_string = str(uuid.uuid4()).replace('-', '')[:length]
+    print(random_string)
+    return random_string
+
+def create_dir(directory_name):
+    # Create the directory
+    try:
+        os.mkdir(directory_name)
+        print(f"Directory '{directory_name}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{directory_name}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{directory_name}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_file_name(file_name_prefix, file_ext):
+    ts = str(get_timestamp())
+    random_chars = get_random_chars()
+    fn = file_name_prefix + ts + random_chars + file_ext
+    return fn
+
+def export_to_file(data, file_name='exported_data', dir_name='exports'):
+    create_dir(dir_name)
+    fn = get_file_name(file_name, '.json')
+    path = os.path.join(dir_name, fn) 
+    f = open(path, "w")
+    f.write(data)
+    f.close()
